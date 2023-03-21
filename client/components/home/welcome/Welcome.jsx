a86cd0efad9943f9c522071e4ca1fcdf9ad4f626
unknown <nurkenkenes7@gmail.com>
2023-03-21 23:09:11 +0600

    implementation of home page welcome section

--- client/components/home/welcome/Welcome.jsx
+++ client/components/home/welcome/Welcome.jsx
@@ -14,7 +14,7 @@ import { icons, SIZES } from '../../../constants'
 
 const jobTypes = ["Full-time", "Part-time", "Contractor"]
 
-const Welcome = () => {
+const Welcome = ({ searchTerm, setSearchTerm, handleClick }) => {
   const router = useRouter()
   const [activeJobType, setActiveJobType] = useState('Full-time')
 
@@ -28,13 +28,13 @@ const Welcome = () => {
         <View style={styles.searchWrapper}>
           <TextInput 
             style={styles.searchInput}
-            value=""
-            onChange={() => {}}
+            value={searchTerm}
+            onChangeText={(text) => setSearchTerm(text)}
             placeholder="What are you looking for?"
           />
         </View>
 
-        <TouchableOpacity style={styles.searchBtn} onPress={() => {}}>
+        <TouchableOpacity style={styles.searchBtn} onPress={handleClick}>
           <Image
             source={icons.search}
             resizeMode="contain"
@@ -46,7 +46,7 @@ const Welcome = () => {
       <View style={styles.tabsContainer}>
         <FlatList
           data={jobTypes}
-          renderItem={({ item }) => {
+          renderItem={({ item }) => (
             <TouchableOpacity
               style={styles.tab(activeJobType, item)}
               onPress={() => {
@@ -56,8 +56,8 @@ const Welcome = () => {
             >
               <Text style={styles.tabText(activeJobType, item)}>{item}</Text>
             </TouchableOpacity>
-          }}
-          keyExtractor={item => item}
+          )}
+          keyExtractor={(item) => item}
           contentContainerStyle={{ columnGap: SIZES.small }}
           horizontal
         />
