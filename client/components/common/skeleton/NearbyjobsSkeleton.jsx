b384380afc7d1d7dc47f1fd3636c6056e3ad4dbe
unknown <nurkenkenes7@gmail.com>
2023-03-23 20:45:18 +0600

    edit NearbyjobsSkeleton component styles

--- client/components/common/skeleton/NearbyjobsSkeleton.jsx
+++ client/components/common/skeleton/NearbyjobsSkeleton.jsx
@@ -1,25 +1,27 @@
 import React from 'react'
-import { View, StyleSheet, Text, Image, TouchableOpacity, Animated, Dimensions } from 'react-native'
-import ContentLoader, { Facebook, Rect, Circle, Path } from 'react-content-loader/native'
+import { View, Dimensions } from 'react-native'
+import ContentLoader, { Rect, Circle } from 'react-content-loader/native'
+
+import styles from './nearbyjobsskeleton.style'
 
 const NearbyJobCardSkeleton = ({ cardWidth }) => {
   return (
     <ContentLoader 
       speed={1}
-      width={cardWidth}
-      height={124}
-      viewBox={`0 0 476 124`}
+      width={cardWidth - 50}
+      height={50}
+      viewBox={`0 0 ${cardWidth} 50`}
       backgroundColor="#f3f3f3"
       foregroundColor="#df2a2a"
     >
-      <Rect x="48" y="8" rx="3" ry="3" width={cardWidth} height="6" /> 
-      <Rect x="48" y="26" rx="3" ry="3" width={cardWidth} height="6" /> 
+      <Rect x="48" y="8" rx="3" ry="3" width={cardWidth  - 50} height="6" /> 
+      <Rect x="48" y="26" rx="3" ry="3" width={cardWidth - 50} height="6" /> 
       <Circle cx="20" cy="20" r="20" />
     </ContentLoader>
   )
 }
 
-const NearbyjobsSkeleton = ({ count = 10 }) => {
+const NearbyjobsSkeleton = ({ count = 15 }) => {
   const cardWidth = Dimensions.get('window').width
   
   const nearbyJobCardSkeletons = Array.from({ length: count }, (_, index) => (
@@ -29,7 +31,7 @@ const NearbyjobsSkeleton = ({ count = 10 }) => {
   ))
   
   return (
-    <View>
+    <View style={styles.container}>
       {nearbyJobCardSkeletons}
     </View>
   )
