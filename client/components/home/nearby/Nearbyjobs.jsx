0288623554fd19cc1e24770a0710be57fd4d5c9f
unknown <nurkenkenes7@gmail.com>
2023-03-25 23:15:28 +0600

    Replaced place where router should follow at Nearbyjobs.jsx

--- client/components/home/nearby/Nearbyjobs.jsx
+++ client/components/home/nearby/Nearbyjobs.jsx
@@ -50,7 +50,7 @@ const Nearbyjobs = () => {
               <NearbyJobCard 
                 job={job}
                 key={`nearby-job-${job?.job_id}`}
-                handleNavigate={() => router.push(`/job-details/${job.job_id}`)}
+                handleNavigate={() => router.push(`../job-details/${job.job_id}`)}
               />
             ))
             ) : (
