205dd47821942b0db933501bbf95f0bc3e8de75c
unknown <nurkenkenes7@gmail.com>
2023-03-26 23:13:36 +0600

    Added sidemenu state, removed prop drill, added closeMenu feature

--- client/app/home/Home.jsx
+++ client/app/home/Home.jsx
@@ -1,47 +1,63 @@
 import { useState } from 'react'
-import { View, ScrollView, SafeAreaView } from 'react-native'
+import { View, ScrollView, SafeAreaView, TouchableOpacity } from 'react-native'
 import { Stack, useRouter } from 'expo-router'
+import { useDispatch, useSelector } from 'react-redux'
 
-import { COLORS, SIZES, icons, images } from '../../constants'
+import { COLORS, SIZES } from '../../constants'
 import { 
   Nearbyjobs,
   Popularjobs,
-  ScreenHeaderBtn,
   Welcome,
   HomeHeader
 } from '../../components'
+import { closeSideMenu } from '../../actions/sidemenu'
 
-const Home = ({ toggleSideMenu }) => {
-  const router = useRouter()
+const Home = () => {
   const [searchTerm, setSearchTerm] = useState("")
+  const { showSideMenu } = useSelector((state) => state.sidemenu)
+
+  const router = useRouter()
+  const dispatch = useDispatch()
+
+  const closeMenu = () => {
+    if(showSideMenu) {
+      dispatch(closeSideMenu())
+    }
+  }
 
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
       <Stack.Screen options={{ headerShown: false }} />
 
-      {/* rewrite property drill to action/reducer */}
-      <HomeHeader toggleSideMenu={toggleSideMenu} />
+      <HomeHeader/>
+      <TouchableOpacity 
+        style={{ opacity: 1 }} 
+        activeOpacity={1} 
+        onPress={closeMenu}
+      >
 
-      <ScrollView showsVerticalScrollIndicator={false}>
-        <View
-          style={{
-            flex: 1,
-            padding: SIZES.medium
-          }}
-        >
-          <Welcome 
-            searchTerm={searchTerm}
-            setSearchTerm={setSearchTerm}
-            handleClick={() => {
-              if(searchTerm) {
-                router.push(`../search/${searchTerm}`)
-              }
+        <ScrollView showsVerticalScrollIndicator={false}>
+          <View
+            style={{
+              flex: 1,
+              padding: SIZES.medium
             }}
-          />
-          <Popularjobs />
-          <Nearbyjobs />
-        </View>
-      </ScrollView>
+          >
+            <Welcome 
+              searchTerm={searchTerm}
+              setSearchTerm={setSearchTerm}
+              handleClick={() => {
+                if(searchTerm) {
+                  router.push(`../search/${searchTerm}`)
+                }
+                closeMenu()
+              }}
+            />
+            <Popularjobs />
+            <Nearbyjobs />
+          </View>
+        </ScrollView>
+      </TouchableOpacity>
     </SafeAreaView>
   )
 }
