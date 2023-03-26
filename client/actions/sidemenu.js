f7bde1e5191c0da11f12ba1d16a1fd8511abe785
unknown <nurkenkenes7@gmail.com>
2023-03-26 23:00:52 +0600

    Added sidemenu close and open actions

---
+++ client/actions/sidemenu.js
@@ -0,0 +1,12 @@
+import {
+  CLOSE_SIDE_MENU,
+  OPEN_SIDE_MENU
+} from '../constants/actionTypes'
+
+export const closeSideMenu = () => ({
+  type: CLOSE_SIDE_MENU
+})
+
+export const openSideMenu = () => ({
+  type: OPEN_SIDE_MENU
+})
