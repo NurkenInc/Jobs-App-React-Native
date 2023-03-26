839e93b413583ccf1058db4ba41564c4094461c3
unknown <nurkenkenes7@gmail.com>
2023-03-26 23:14:20 +0600

    Removed prop drill and usage of useSideMenu hook

--- client/app/index.js
+++ client/app/index.js
@@ -3,19 +3,13 @@ import { Provider } from 'react-redux'
 import store from '../state/store'
 import Home from './home/Home'
 import { SideMenu } from '../components'
-import useSideMenu from '../hook/useSideMenu'
 
 const App = () => {
-  const { showSideMenu, toggleSideMenu, menuAnimation } = useSideMenu()
 
   return (
     <Provider store={store}>
-      <Home toggleSideMenu={toggleSideMenu} />
-      <SideMenu 
-        showSideMenu={showSideMenu} 
-        toggleSideMenu={toggleSideMenu} 
-        menuAnimation={menuAnimation}
-      />
+      <Home />
+      <SideMenu />
     </Provider>
   )
 }
