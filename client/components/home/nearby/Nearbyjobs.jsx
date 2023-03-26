a3e6cdc684b02882784b540b34db2109e0b6e9ba
unknown <nurkenkenes7@gmail.com>
2023-03-26 23:12:01 +0600

    Removed prop drill, added serach by country feature

--- client/components/home/nearby/Nearbyjobs.jsx
+++ client/components/home/nearby/Nearbyjobs.jsx
@@ -1,8 +1,10 @@
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { useRouter } from 'expo-router'
 import { View, Text, TouchableOpacity } from 'react-native'
 import debounce from 'lodash.debounce'
+import * as Location from 'expo-location'
+import axios from 'axios'
 
 import styles from './nearbyjobs.style'
 
@@ -10,23 +12,94 @@ import NearbyjobsSkeleton from '../../common/skeleton/NearbyjobsSkeleton'
 import NearbyJobCard from '../../common/cards/nearby/NearbyJobCard'
 
 import { fetchNearbyJobs } from '../../../actions/jobs'
+import { closeSideMenu } from '../../../actions/sidemenu'
+
+import { RAPID_GEOCODE_API_KEY } from '@env'
+
+const rapidGeocodeApiKey = RAPID_GEOCODE_API_KEY
 
 const Nearbyjobs = () => {
-  const router = useRouter()
-
-  const dispatch = useDispatch()
+  const [country, setCountry] = useState('')
+  const [location, setLocation] = useState({})
   const { data, error } = useSelector((state) => state.jobs.nearbyJobs)
+  const { showSideMenu } = useSelector((state) => state.sidemenu)
+  
+  const router = useRouter()
+  const dispatch = useDispatch()
 
   const fetchData
    = debounce(() => {
     dispatch(fetchNearbyJobs({
       endpoint: 'search',
       query: {
-        query: 'React Native developer',
+        query: `React Native developer ${country}`,
         num_pages: 1
       }
     }))
   }, 5000)
+
+  const closeMenu = () => {
+    if (showSideMenu) {
+      dispatch(closeSideMenu())
+    }
+  }
+
+  const handleCardPress = (job) => {
+    router.push(`../job-details/${job.job_id}`)
+    closeMenu()
+  }
+
+  // rewrite location country logic to reducers and actions
+  const getCurrentCountry = async ({ lat, lon }) => {
+    const options = {
+      method: 'GET',
+      url: 'https://forward-reverse-geocoding.p.rapidapi.com/v1/reverse',
+      params: {
+        lat,
+        lon,
+        'accept-language': 'en',
+        polygon_threshold: '0.0'
+      },
+      headers: {
+        'X-RapidAPI-Key': rapidGeocodeApiKey,
+        'X-RapidAPI-Host': 'forward-reverse-geocoding.p.rapidapi.com'
+      }
+    }
+
+    try {
+      const response = await axios.request(options)
+
+      if(response.data.address.country) {
+        setCountry(response.data.address.country)
+      } else {
+        setCountry('')
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const getCurrentLocation = () => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync()
+
+      if(status !== 'granted') {
+        return
+      }
+
+      let location = await Location.getCurrentPositionAsync({})
+
+      const lat = location.coords.latitude
+      const lon = location.coords.longitude
+
+      setLocation({ lat, lon })
+    })()
+  }
+
+  useEffect(() => {
+    getCurrentLocation()
+    getCurrentCountry(location)
+  }, [])
 
   useEffect(() => {
     fetchData()
@@ -50,7 +123,9 @@ const Nearbyjobs = () => {
               <NearbyJobCard 
                 job={job}
                 key={`nearby-job-${job?.job_id}`}
-                handleNavigate={() => router.push(`../job-details/${job.job_id}`)}
+                handleNavigate={() => {
+                  handleCardPress(job)
+                }}
               />
             ))
             ) : (
