700ae4fde4a8775c12e8df2e25a5bb1db9903a32
unknown <nurkenkenes7@gmail.com>
2023-03-26 23:02:11 +0600

    Added sidemenu reducer to combined reducer

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -1,7 +1,8 @@
 import { combineReducers } from 'redux'
 
 import jobs from './jobs'
+import sidemenu from './sidemenu'
 
-const reducers = combineReducers({ jobs })
+const reducers = combineReducers({ jobs, sidemenu })
 
 export default reducers
