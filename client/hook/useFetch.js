be413164381043502b882cfa3cfddab8b1202ecd
unknown <nurkenkenes7@gmail.com>
2023-03-26 23:04:20 +0600

    Renamed env variable

--- client/hook/useFetch.js
+++ client/hook/useFetch.js
@@ -1,21 +1,21 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 
-import { RAPID_API_KEY } from "@env" 
+import { RAPID_JSEARCH_API_KEY } from "@env" 
 
-const rapidApiKey = RAPID_API_KEY
+const rapidJSearchApiKey = RAPID_JSEARCH_API_KEY
 
 const useFetch = (endpoint, query) => {
   const [data, setData] = useState([])
   const [isLoading, setIsLoading] = useState(false)
   const [error, setError] = useState(null)
 
-  //
+  ////
   
   const options = {
     method: 'GET',
     headers: {
-      'X-RapidAPI-Key': rapidApiKey,
+      'X-RapidAPI-Key': rapidJSearchApiKey,
       'X-RapidAPI-Host': 'jsearch.p.rapidapi.com'
     },
     url: `https://jsearch.p.rapidapi.com/${endpoint}`,
