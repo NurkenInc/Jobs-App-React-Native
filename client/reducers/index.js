a443c3a62612dd03f43fc958c3ef7f56d4faa59b
unknown <nurkenkenes7@gmail.com>
2023-03-27 22:55:44 +0600

    Combined location, locationPermission, contry reducers to main reducer

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -2,7 +2,10 @@ import { combineReducers } from 'redux'
 
 import jobs from './jobs'
 import sidemenu from './sidemenu'
+import location from './location'
+import locationPermission from './locationPermission'
+import country from './country'
 
-const reducers = combineReducers({ jobs, sidemenu })
+const reducers = combineReducers({ jobs, sidemenu, location, locationPermission, country })
 
 export default reducers
