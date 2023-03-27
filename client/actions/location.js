3404e120ce2084fdf4f985939785aa5e30eda3c4
unknown <nurkenkenes7@gmail.com>
2023-03-27 22:50:28 +0600

    Added location determination feature to reducers and actions

---
+++ client/actions/location.js
@@ -0,0 +1,27 @@
+import * as Location from 'expo-location'
+
+import {
+  FETCH_LOCATION_REQUEST,
+  FETCH_LOCATION_SUCCESS,
+  FETCH_LOCATION_FAILURE,
+} from '../constants/actionTypes'
+
+export const getLocation = () => {
+  return async (dispatch) => {
+    dispatch({ type: FETCH_LOCATION_REQUEST })
+
+    try {
+      const { coords } = await Location.getCurrentPositionAsync({})
+
+      const { latitude, longitude } = coords
+
+      dispatch({
+        type: FETCH_LOCATION_SUCCESS,
+        payload: { latitude, longitude }
+      })
+
+    } catch (error) {
+      dispatch({ type: FETCH_LOCATION_FAILURE, payload: error })
+    }
+  }
+}
---
+++ client/reducers/location.js
@@ -0,0 +1,37 @@
+import {
+  FETCH_LOCATION_REQUEST,
+  FETCH_LOCATION_SUCCESS,
+  FETCH_LOCATION_FAILURE
+} from '../constants/actionTypes'
+
+const initialState = {
+  isLoading: false,
+  data: {},
+  error: null
+}
+
+const location = (state = initialState, action) => {
+  switch (action) {
+    case FETCH_LOCATION_REQUEST:
+      return {
+        ...state,
+        isLoading: true
+      }
+    case FETCH_LOCATION_SUCCESS:
+      return {
+        ...state,
+        isLoading: false,
+        data: action.payload
+      }
+    case FETCH_LOCATION_FAILURE:
+      return {
+        ...state,
+        isLoading: false,
+        error: action.payload
+      }
+    default:
+      return state
+  }
+}
+
+export default location
