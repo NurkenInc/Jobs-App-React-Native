a86113a032a860787125f2796a762196bae06dc2
unknown <nurkenkenes7@gmail.com>
2023-03-30 22:47:04 +0600

    Removed extra states and debug console logs

--- client/actions/locationPermission.js
+++ client/actions/locationPermission.js
@@ -3,24 +3,18 @@ import * as Location from 'expo-location'
 import {
   LOCATION_PERMISSION_REQUEST,
   LOCATION_PERMISSION_GRANTED,
-  LOCATION_PERMISSION_UNDETERMINED,
-  LOCATION_PERMISSION_DENIED,
   LOCATION_PERMISSION_FAILURE
 } from '../constants/actionTypes'
 
 export const getLocationPermission = () => {
   return async (dispatch) => {
     dispatch({ type: LOCATION_PERMISSION_REQUEST })
-    
+
     try {
-      let { status } = await Location.getForegroundPermissionsAsync()
+      let { status } = await Location.requestForegroundPermissionsAsync()
       
       if (status === 'granted') {
         dispatch({ type: LOCATION_PERMISSION_GRANTED })
-      } else if (status === 'denied') {
-        dispatch({ type: LOCATION_PERMISSION_DENIED })
-      } else {
-        dispatch({ type: LOCATION_PERMISSION_UNDETERMINED })
       }
     } catch (error) {
       dispatch({ type: LOCATION_PERMISSION_FAILURE, payload: error })
